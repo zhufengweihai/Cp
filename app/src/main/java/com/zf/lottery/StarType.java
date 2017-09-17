4593bda3a3a433601f6cafe21ac0f3554fa2bd0e
zhufengweihai <zhufengweihai@gmail.com>
2017-09-18 07:49:37 +0800

    修改代码bug；类型

--- app/src/main/java/com/zf/lottery/StarType.java
+++ app/src/main/java/com/zf/lottery/StarType.java
@@ -1,8 +1,7 @@
 package com.zf.lottery;
 
 public enum StarType {
-    FirstThree("前三位"), LastThree("后三位"), FirstTwo("前二位"), LastTwo("后二位"), CombThree("组选后三"), CombTwo("组选后二"), LastOne
-            ("后一位");
+    FirstThree("前后三"), LastThree("后三位"), FirstTwo("前后二"), LastTwo("后二位"), GroupThree("三星组三"), GroupSix("三星组六"), CombTwo("组选后二"), LastOne("单选后一");
 
     StarType(String name) {
         this.name = name;
